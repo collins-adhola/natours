88786424ec83c045b0e98c2a7e9b271f1c4343da
Collino <coadhola@gmail.com>
2022-06-18 15:45:31 +0100

    filtering operator for routes

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from kampala middlewear');
   next();
 });
 
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,20 +21,29 @@ const Tour = require('./../models/tourModel'); // got from model going to be con
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
+
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
+    
 
-    const query = Tour.find(queryObj);
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+     console.log(JSON.parse(queryStr));
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // EXECUTE QUERY
+    const tours = await query;
 
     // const query =  Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    // EXECUTE QUERY
-    const tours = await query;
 
     //SEND RESPONSE
     res.status(200).json({
@@ -46,7 +55,7 @@ exports.getAllTours = async (req, res) => {
     });
   } catch (err) {
     res.status(404).json({
-      status: 'fail',
+      status: 'failed',
       message: err,
     });
   }
